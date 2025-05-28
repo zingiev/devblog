e26da3556f040953b36e2b6326031813f61d42b0
zingiev <m@zingiev.ru>
2025-05-28 21:23:15 +0300

    add middleware

--- devblog/settings.py
+++ devblog/settings.py
@@ -56,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'devblog.middleware.HiddenUrlAdmin',
 ]
 
 ROOT_URLCONF = 'devblog.urls'
