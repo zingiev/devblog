c3c64cb54bf49c81bf7a412a079399bd13660bb9
zingiev <m@zingiev.ru>
2025-05-28 21:25:31 +0300

    delete middleware

--- devblog/settings.py
+++ devblog/settings.py
@@ -56,8 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-    'devblog.middleware.HiddenUrlAdmin',
 ]
 
 ROOT_URLCONF = 'devblog.urls'
